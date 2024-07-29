4941eb5f83fd364cad1fa67511e6b4affce257ed
Michael Götte <36420750+RoteKekse@users.noreply.github.com>
2024-07-29 21:50:19 +0200

    Update __init__.py

--- src/nomad_hiern/schema_packages/__init__.py
+++ src/nomad_hiern/schema_packages/__init__.py
@@ -4,7 +4,7 @@ from nomad.config.models.plugins import SchemaPackageEntryPoint
 class HIERNPackageEntryPoint(SchemaPackageEntryPoint):
 
     def load(self):
-        from nomad_hysprint.schema_packages.hiern_package import m_package
+        from nomad_hiern.schema_packages.hiern_package import m_package
         return m_package
 
 
